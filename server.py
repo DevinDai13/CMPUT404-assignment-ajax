dedcf0e05f407f26f01b1a630a0f25cc62e09b63
Devin <devin@DESKTOP-OL5E28E.localdomain>
2020-02-19 12:19:39 -0700

    initial

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-# Copyright 2013 Abram Hindle
+# Copyright 2020 Abram Hindle, Deivn Dai
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,9 +22,9 @@
 
 
 import flask
-from flask import Flask, request
+from flask import Flask, request, redirect
 import json
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 app.debug = True
 
 # An example world
@@ -74,27 +74,46 @@ def flask_post_json():
 @app.route("/")
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return flask.redirect('static/index.html')
 
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    content = flask_post_json()
+    myWorld.set(entity, content)
+    response = app.response_class(
+        response=json.dumps(content),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    response = app.response_class(
+        response=json.dumps(myWorld.world()),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    response = app.response_class(
+        response=json.dumps(myWorld.get(entity)),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return 'cleared'
 
 if __name__ == "__main__":
     app.run()
